345e074d6936c684417fd6d129d561d7028c6e37
german23.03.09@gmai.com <german23.03.09@gmail.com>
2025-07-21 21:11:45 -0300

    se agrego el clousure

--- src/js/products.js
+++ src/js/products.js
@@ -7,29 +7,47 @@ const searchInput = document.querySelector('.searcher__word');
 const searchBtn = document.querySelector('.button--search');
 const allBtn = document.querySelector('.button--all');
 
-console.log('gallery:', gallery);
-console.log('searchInput:', searchInput);
-console.log('searchBtn:', searchBtn);
-console.log('allBtn:', allBtn);
+// Closure fuera de renderProducts
+function createCounter(counterElem) {
+  let count = 0;
+  return {
+    add: () => {
+      count++;
+      counterElem.textContent = count;
+    },
+    remove: () => {
+      if (count > 0) count--;
+      counterElem.textContent = count;
+    }
+  };
+}
 
 function renderProducts(productList) {
   console.log('Renderizando productos:', productList);
   gallery.innerHTML = '';
   productList.forEach(product => {
-    gallery.innerHTML += `
-      <div class="card">
-        <img class="card__image" src="${product.image}" alt="${product.name}">
-        <h3 class="card__title">${product.name}</h3>
-        <span class="card__code">${product.code}</span>
-        <p class="card__description">${product.description}</p>
-        <span class="card__price">$${product.price}</span>
-        <div class="card__actions">
-          <button class="button button--add">Agregar al carrito</button>
-          <span class="card__counter">0</span>
-          <button class="button button--remove">Quitar del carrito</button>
-        </div>
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.innerHTML = `
+      <img class="card__image" src="${product.image}" alt="${product.name}">
+      <h3 class="card__title">${product.name}</h3>
+      <span class="card__code">${product.code}</span>
+      <p class="card__description">${product.description}</p>
+      <span class="card__price">$${product.price}</span>
+      <div class="card__actions">
+        <button class="button button--add">Agregar al carrito</button>
+        <span class="card__counter">0</span>
+        <button class="button button--remove">Quitar del carrito</button>
       </div>
     `;
+    const addBtn = card.querySelector('.button--add');
+    const removeBtn = card.querySelector('.button--remove');
+    const counterElem = card.querySelector('.card__counter');
+    // Usar el closure externo para el contador
+    const counter = createCounter(counterElem);
+    addBtn.addEventListener('click', counter.add);
+    removeBtn.addEventListener('click', counter.remove);
+    gallery.appendChild(card);
   });
 }
 
@@ -58,4 +76,4 @@ allBtn.addEventListener('click', () => {
 });
 
 // Render inicial
-renderProducts(products); 
+renderProducts(products);
